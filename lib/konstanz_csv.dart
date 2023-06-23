bf69c3214b526749e9df29c91c32a1172b3d5d9a
Maximilian Böhmichen <max@boehmichen.de>
2023-06-23 21:59:38 +0200

    added nodes

---
+++ lib/konstanz_csv.dart
@@ -0,0 +1,27 @@
+const String separator = ';';
+
+const String konstanzNodes = """name,floor,type,keywords
+Immobetriebe Empfang,0,,
+Immobetriebe Buero,0,,
+Farm Kueche,0,room,0.01
+Farm Buero Annabell Schaefer und Stephanie Hoffmann,0,,0.02
+Farm Buero Nicola Voigt,0,,0.03
+Farm Buero Christina Groll,0,,0.04
+Monumentum,0,,0.05
+Georgs Genuss Manufaktur,0,,0.06
+Georgs Genuss Manufaktur,0,,0.07
+Georgs Genuss Manufaktur,0,,0.08
+jetzt-GmbH,0,,0.09
+Eingangshalle,0,,
+Chill Area,0,,
+Gründungszimmer,0,,0.16
+energy depot,0,,0.15
+Laibmann,0,,0.14
+farm,0,,0.10
+Organi-Farms,0,,0.11
+Organi-Farms,0,,0.12
+Toilette Maenner,0,,
+Toilette Frauen,0,,
+Aufzug,0,,
+""";
+const String konstanzEdges = "from,to,weight";
