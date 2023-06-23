69407b7c85022296be815d86b95dbc585ff77c48
Maximilian Böhmichen <max@boehmichen.de>
2023-06-23 22:03:20 +0200

    added nodes-update

--- lib/konstanz_csv.dart
+++ lib/konstanz_csv.dart
@@ -1,27 +1,28 @@
 const String separator = ';';
 
 const String konstanzNodes = """name,floor,type,keywords
-Immobetriebe Empfang,0,,
-Immobetriebe Buero,0,,
-Farm Kueche,0,room,0.01
-Farm Buero Annabell Schaefer und Stephanie Hoffmann,0,,0.02
-Farm Buero Nicola Voigt,0,,0.03
-Farm Buero Christina Groll,0,,0.04
-Monumentum,0,,0.05
-Georgs Genuss Manufaktur,0,,0.06
-Georgs Genuss Manufaktur,0,,0.07
-Georgs Genuss Manufaktur,0,,0.08
-jetzt-GmbH,0,,0.09
-Eingangshalle,0,,
-Chill Area,0,,
-Gründungszimmer,0,,0.16
-energy depot,0,,0.15
-Laibmann,0,,0.14
-farm,0,,0.10
-Organi-Farms,0,,0.11
-Organi-Farms,0,,0.12
-Toilette Maenner,0,,
-Toilette Frauen,0,,
-Aufzug,0,,
+name,floor,type,barrier-free,keywords
+Immobetriebe Empfang,0,hallway,TRUE,
+Immobetriebe Büro,0,room,TRUE,
+Farm Kueche,0,room,TRUE,0.01
+Farm Buero Annabell Schaefer und Stephanie Hoffmann,0,room,TRUE,0.02
+Farm Buero Nicola Voigt,0,room,TRUE,0.03
+Farm Buero Christina Groll,0,room,TRUE,0.04
+Monumentum,0,room,TRUE,0.05
+Georgs Genuss Manufaktur,0,room,TRUE,0.06
+Georgs Genuss Manufaktur,0,room,TRUE,0.07
+Georgs Genuss Manufaktur,0,room,TRUE,0.08
+jetzt-GmbH,0,room,TRUE,0.09
+Eingangshalle,0,hallway,TRUE,
+Chill Area,0,hallway,TRUE,
+Gründungszimmer,0,room,TRUE,0.16
+energy depot,0,room,TRUE,0.15
+Laibmann,0,room,TRUE,0.14
+farm,0,room,TRUE,0.10
+Organi-Farms,0,room,TRUE,0.11
+Organi-Farms,0,room,TRUE,0.12
+Toilette Maenner,0,toilet,TRUE,
+Toilette Frauen,0,toilet,TRUE,
+Aufzug,0,elevator,TRUE,
 """;
 const String konstanzEdges = "from,to,weight";
